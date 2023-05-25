8da2cf3c0e35fabeab1097bb9e2254b3f7565bf8
daeunkwak <misodaeun3@naver.com>
2023-05-25 16:18:02 +0900

    swagger

--- src/main/java/app/gangdan/please/api/HelloController.java
+++ src/main/java/app/gangdan/please/api/HelloController.java
@@ -1,5 +1,7 @@
 package app.gangdan.please.api;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,14 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @Tag(name = "hello", description = "테스트 API")
+@Tag(name = "hello", description = "테스트 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/hello")
 public class HelloController {
 
-    // @Tag(name = "hello")
-    // @ApiOperation(value = "test!!")
+    @Tag(name = "hello")
+    @ApiOperation(value = "test!!")
     @GetMapping("")
     public ResponseEntity<?> testHello(){
 
