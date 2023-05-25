fc5cdeb6b0d268bbcdf8a814a0f0fb9fa0047cd6
daeunkwak <misodaeun3@naver.com>
2023-05-25 15:24:35 +0900

    1

---
+++ src/main/java/app/gangdan/please/api/HelloController.java
@@ -0,0 +1,23 @@
+package app.gangdan.please.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Tag(name = "hello", description = "테스트 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    // @Tag(name = "hello")
+    // @ApiOperation(value = "test!!")
+    @GetMapping("")
+    public ResponseEntity<?> testHello(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
