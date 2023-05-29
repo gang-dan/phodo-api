c8b95c6c3d36e06b72cf73851193f1b4a2c66d2a
daeunkwak <misodaeun3@naver.com>
2023-05-30 00:17:11 +0900

    feat: remote 변경

--- src/main/java/app/gangdan/please/api/OauthLoginController.java
+++ src/main/java/app/gangdan/please/api/OauthLoginController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @Slf4j
